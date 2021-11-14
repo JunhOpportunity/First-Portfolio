c6cc9c6844e60e7fca8512c77148261c8f15aee9
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2021-11-15 00:24:42 +0900

    JS scrollIntoView Smooth

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 'use strict';
 
-function printhellojava (){
-    console.log("Hello world")
-}
+// Make navbasr transparent when it is on the top
+const navbar = document.querySelector('#navbar');
+const navberHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    if(window.scrollY > navberHeight) {
+        navbar.classList.add('navbar--dark');
+    }
+    else {
+        navbar.classList.remove('navbar--dark');
+    }
+});
 
-printhellojava();
 
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null) {
+        return;
+    }
+    console.log(event.target.dataset);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+});
