5b7be9bc08745fed2cbedb3fa7673e85a8c99d81
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2021-11-16 00:58:44 +0900

    JS (Contact me & Arrow Up & Fade & Projects) ADD

--- main.js
+++ main.js
@@ -21,7 +21,67 @@ navbarMenu.addEventListener('click', (event) => {
     if(link == null) {
         return;
     }
-    console.log(event.target.dataset);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: "smooth"});
+    scrollIntoView(link);
 });
+
+// Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+    scrollIntoView('#contact');
+})
+
+// Make home slowlyfade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+})
+
+// Show "arrow up" button when scroling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if(window.scrollY > homeHeight / 2) {
+        arrowUp.classList.add('visible');
+    }
+    else {
+        arrowUp.classList.remove('visible');
+    }
+})
+
+// Handle click on the "Arrow up" button
+arrowUp.addEventListener('click', () => {
+    scrollIntoView('#home');
+})
+
+
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter; /* 만약 버튼의 숫자를 누른 경우라도 반응하도록. (잘 이해 x) */
+    if(filter == null) {
+        return;
+    }
+    projectContainer.classList.add('anim-out');
+    
+
+    setTimeout(() => {
+        projects.forEach((project) => { // forEach 구문은 let a of array 와 같음 
+            console.log(project.dataset.type);
+    
+            if (filter === '*' || filter === project.dataset.type) {
+                project.classList.remove('invisible');
+            }
+            else {
+                project.classList.add('invisible');
+            }
+        });
+        projectContainer.classList.remove('anim-out');
+    }, 300); // 300ms 이후에 다시 삭제
+});
+
+function scrollIntoView(selector) { // 원하는 위치로 이동하는 함수
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+}
